0af25f08d5df494596a771181afbbabfdb8b08a8
Gianluca Fiore <forod.g@gmail.com>
2009-12-24 14:36:44 +0100

    switch to chromium in extra instead than the one in aur

--- rc.lua
+++ rc.lua
@@ -270,7 +270,7 @@ mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, image(home .. "/
                                         { "open terminal", terminal, image("/usr/share/pixmaps/rxvt-unicode.png") },
                                         { "firefox (navigation)", "firefox -P navigation3 --no-remote", image("/usr/share/pixmaps/firefox.png") },
                                         { "firefox (maidens)", "firefox -P maidens3 --no-remote", image("/usr/share/pixmaps/firefox.png") },
-                                        { "Chromium", "chromium-browser", image("/usr/share/pixmaps/chromium-browser.png") },
+                                        { "Chromium", "chromium", image("/usr/share/icons/hicolor/32x32/apps/chromium.png") },
                                         { "thunar", "thunar", image(home .. "/.icons/thunar-logo.png") },
                                         { "Music", "gmusicbrowser", image("/usr/share/pixmaps/gmusicbrowser.png") },
                                         { "Pidgin", "pidgin", image("/usr/share/icons/hicolor/16x16/apps/pidgin.png") },
