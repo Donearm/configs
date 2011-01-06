af0f4536aad4a1757ce7d97c2bc0e403e0e46b19
Gianluca Fiore <forod.g@gmail.com>
2011-01-06 12:30:20 +0100

    disabled D keybind, too easy to fatfinger it

--- keys.py
+++ keys.py
@@ -4,6 +4,6 @@ map = KeyMapWithDirections()
 keymanager.get_context('console')
 
 # delete marked files right away
-map('D', fm.execute_console("shell -d rm -rf %s"))
+#map('D', fm.execute_console("shell -d rm -rf %s"))
 
 keymanager.merge_all(map)
