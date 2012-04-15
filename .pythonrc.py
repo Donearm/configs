5c70f88e609763b4c97de7373487bde0ecc769dd
Gianluca Fiore <forod.g@gmail.com>
2012-04-15 12:20:13 +0200

    .pythonrc.py is correctly revisioned now

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,1 +1,7 @@
-/home/gianluca/.pythonrc.py
+try:
+    import readline
+except ImportError:
+    print("Module readline not available")
+else:
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
