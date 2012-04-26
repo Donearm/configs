ac34179bd8051d52514a26e459ed0d0b4b984a8e
Gianluca Fiore <forod.g@gmail.com>
2012-04-26 17:08:56 +0200

    removed apps.py and keys.py (no more used/necessary)

--- .config/ranger/keys.py
+++
@@ -1,9 +0,0 @@
-from ranger.api.keys import *
-
-map = KeyMapWithDirections()
-keymanager.get_context('console')
-
-# delete marked files right away
-#map('D', fm.execute_console("shell -d rm -rf %s"))
-
-keymanager.merge_all(map)
