e4f4e5d376753c477b20f7883512b5c0e39ff6ca
Gianluca Fiore <forod.g@gmail.com>
2015-12-27 13:55:33 +0100

    new AwesomeWM theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -75,7 +75,7 @@ local mutt = terminal .. " -e mutt -y"
 local maildir = home .. "/Maildir"
 local lockScreen = "slock"
 -- Themes define colours, icons, and wallpapers
-theme_path = home .. "/.config/awesome/themes/agios_georgios_beach"
+theme_path = home .. "/.config/awesome/themes/naked_apron"
 -- Actually load theme
 beautiful.init(theme_path)
 -- Define if we want to see naughty notifications
