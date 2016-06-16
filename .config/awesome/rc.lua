76638e78b509490abfe926cdfda15902533db9e3
Gianluca Fiore <forod.g@gmail.com>
2016-06-16 12:17:10 +0200

    add brave browser to the menu

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -172,6 +172,7 @@ vboxmenu = {
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesomewm_image },
                                         { "open terminal", terminal, beautiful.terminal_image },
                                         { "firefox", browser, beautiful.firefox_image },
+										{ "brave", "brave", beautiful.brave_image },
 --                                        { "Chrome", browser, beautiful.chrome_image },
                                         { "ranger", filemanager, beautiful.filemanager_image },
                                         { "Music", music, beautiful.music_image },
