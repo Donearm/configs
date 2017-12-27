3bce4cefb33ee1f63b934977e854e7e270d3f7b5
Gianluca Fiore <forod.g@gmail.com>
2017-12-27 19:10:48 +0100

    fix changing layout function

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -432,10 +432,10 @@ awful.screen.connect_for_each_screen(function(s)
 	-- We need one layoutbox per screen.
 	s.mylayoutbox = awful.widget.layoutbox(s)
 	s.mylayoutbox:buttons(awful.util.table.join(
-			awful.button({ }, 1, function () awful.layout.inc(layouts, 1) end),
-			awful.button({ }, 3, function () awful.layout.inc(layouts, -1) end),
-			awful.button({ }, 4, function () awful.layout.inc(layouts, 1) end),
-			awful.button({ }, 5, function () awful.layout.inc(layouts, -1) end)))
+			awful.button({ }, 1, function () awful.layout.inc(1) end),
+			awful.button({ }, 3, function () awful.layout.inc(-1) end),
+			awful.button({ }, 4, function () awful.layout.inc(1) end),
+			awful.button({ }, 5, function () awful.layout.inc(-1) end)))
 	-- Create a taglist widget
 	s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, taglist_buttons)
 
@@ -565,8 +565,8 @@ globalkeys = gears.table.join(
 	awful.key({ modkey, "Shift"   }, "l", function () awful.tag.incnmaster(-1)      end),
 	awful.key({ modkey, "Control" }, "h", function () awful.tag.incncol( 1)         end),
 	awful.key({ modkey, "Control" }, "l", function () awful.tag.incncol(-1)         end),
-	awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-	awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+	awful.key({ modkey,           }, "space", function () awful.layout.inc(1) end),
+	awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1) end),
 
 	awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
