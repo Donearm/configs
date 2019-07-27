57f9a839906803ebc9555b6fb36f3ec7d8d653f5
Gianluca Fiore <gianlucafiore@papersounds.eu>
2019-07-27 11:17:17 +0200

    add handle for raw images in scope.sh (for previews)

--- .config/ranger/scope.sh
+++ .config/ranger/scope.sh
@@ -108,6 +108,17 @@ handle_image() {
         #           && exit 6 || exit 1;;
 
         # Image
+        image/x-canon-cr2|image/x-olympus-orf|image/x-adobe-dng|image/tiff)
+            local orientation
+            # extract orientation from RAW file using exiftool (identify won't work)
+            orientation=$( exiftool -b -Orientation "${FILE_PATH}")
+            exiftool -b -PreviewImage "${FILE_PATH}" > "${IMAGE_CACHE_PATH}"
+            if [[ -n "$orientation" && "$orientation" != 1 ]]; then
+                # ...auto-rotate the image according to the EXIF data.
+                exiftool -overwrite_original_in_place -Orientation="$orientation" -n "${IMAGE_CACHE_PATH}"
+                mogrify -auto-orient "${IMAGE_CACHE_PATH}"
+            fi
+            exit 6;;
         image/*)
             local orientation
             orientation="$( identify -format '%[EXIF:Orientation]\n' -- "${FILE_PATH}" )"
